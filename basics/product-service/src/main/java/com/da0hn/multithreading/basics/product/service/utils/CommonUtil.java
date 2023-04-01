03a2b8b068ead9a59cfa5d4755019809cd1bf90c
Da0hn <gabrielhondacba@gmail.com>
2023-04-01 12:47:56 -0400

    feat: add common utility functions

---
+++ basics/product-service/src/main/java/com/da0hn/multithreading/basics/product/service/utils/CommonUtil.java
@@ -0,0 +1,43 @@
+package com.da0hn.multithreading.basics.product.service.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.time.StopWatch;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CommonUtil {
+
+  private static final StopWatch stopWatch = new StopWatch();
+  private static final int TRANSFORM_DELAY = 500;
+
+  public static void delay(final int delayInMilliseconds) {
+    try {
+      Thread.sleep(delayInMilliseconds);
+    } catch (final InterruptedException e) {
+      LoggerUtil.log("Exception occurs: " + e.getMessage());
+    }
+  }
+
+  public static String transform(final String text) {
+    CommonUtil.delay(TRANSFORM_DELAY);
+    return text.toUpperCase();
+  }
+
+  public static void startTimer() {
+    stopWatch.start();
+  }
+
+  public static void timeElapsed() {
+    stopWatch.stop();
+    LoggerUtil.log("Total time elapsed " + stopWatch.getTime() + "ms");
+  }
+
+  public static void resetTimer() {
+    stopWatch.reset();
+  }
+
+  public static int numberOfCores() {
+    return Runtime.getRuntime().availableProcessors();
+  }
+
+}
