c58d5794e01c09f4cf6d31ce56fb13977b26da12
Da0hn <gabrielhondacba@gmail.com>
2023-04-01 12:47:03 -0400

    fix: make `LoggerUtil` non-instantiable

--- basics/product-service/src/main/java/com/da0hn/multithreading/basics/product/service/utils/LoggerUtil.java
+++ basics/product-service/src/main/java/com/da0hn/multithreading/basics/product/service/utils/LoggerUtil.java
@@ -1,5 +1,9 @@
 package com.da0hn.multithreading.basics.product.service.utils;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class LoggerUtil {
 
   public static void log(final String message) {
