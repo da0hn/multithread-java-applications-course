3fe2a7fe638f6613cffa4b08f6ee11c72b721eb8
Da0hn <gabrielhondacba@gmail.com>
2023-04-01 20:32:54 -0400

    test: add test to modify `parallelism` behaviour

--- basics/checkout-service/src/test/java/com/da0hn/multithreading/basics/checkout/service/CheckoutServiceTest.java
+++ basics/checkout-service/src/test/java/com/da0hn/multithreading/basics/checkout/service/CheckoutServiceTest.java
@@ -16,6 +16,7 @@ class CheckoutServiceTest {
 
   @BeforeEach
   void setUp() {
+    System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", String.valueOf(Runtime.getRuntime().availableProcessors()));
     this.sut = new CheckoutService(new PriceValidatorService());
   }
 
@@ -49,4 +50,23 @@ class CheckoutServiceTest {
       .satisfies(checkoutFail, hasInvalidItem);
 
   }
+
+
+  @Test
+  @DisplayName("Should change parallelism workers count")
+  void test3() {
+
+    System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "100");
+
+    final var cart = CartDataSet.createCart(100);
+    final var response = this.sut.checkout(cart);
+
+    final Consumer<CheckoutResponse> checkoutFail = r -> Assertions.assertThat(r.status()).isEqualTo(CheckoutStatus.FAILURE);
+    final Consumer<CheckoutResponse> hasInvalidItem = r -> Assertions.assertThat(r.invalidItems()).hasSize(3);
+
+    Assertions.assertThat(response)
+      .satisfies(checkoutFail, hasInvalidItem);
+
+  }
+
 }
