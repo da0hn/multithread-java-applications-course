fae311400b431a5093d579e888671c315419e586
Da0hn <gabrielhondacba@gmail.com>
2023-04-21 11:12:28 -0400

    feat: add example of `thenApply` with `supplyAsync`

---
+++ basics/completable-future-sandbox/src/main/java/com/da0hn/multithreading/completable/future/sandbox/SupplyAsyncAndThenApplyExample.java
@@ -0,0 +1,22 @@
+package com.da0hn.multithreading.completable.future.sandbox;
+
+import com.da0hn.multithreading.commons.utils.LoggerUtil;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.CompletableFuture;
+
+public final class SupplyAsyncAndThenApplyExample {
+
+  @SneakyThrows
+  public static void main(final String[] args) {
+    final var service = new HelloWorldService();
+
+    CompletableFuture.supplyAsync(service::helloWorld) // delayed by 1s
+      .thenApply(String::toUpperCase)
+      .thenAccept(LoggerUtil::log) // perform action with supplied value
+      .join(); // blocking main thread
+
+    LoggerUtil.log("Done!");
+  }
+
+}
