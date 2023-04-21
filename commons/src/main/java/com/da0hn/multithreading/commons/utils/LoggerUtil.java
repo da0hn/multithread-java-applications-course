a4d48ee0d801f6adff1fc8b79036deb82fd7cb14
Da0hn <gabrielhondacba@gmail.com>
2023-04-21 11:04:26 -0400

    refactor: show current time in `LoggerUtil`

--- commons/src/main/java/com/da0hn/multithreading/commons/utils/LoggerUtil.java
+++ commons/src/main/java/com/da0hn/multithreading/commons/utils/LoggerUtil.java
@@ -3,11 +3,17 @@ package com.da0hn.multithreading.commons.utils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class LoggerUtil {
 
   public static void log(final Object message) {
-    System.out.println("[" + Thread.currentThread().getName() + "] - " + message);
+    final var timeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.SSSS");
+    final var now = LocalDateTime.now();
+    final var threadName = Thread.currentThread().getName();
+    System.out.printf("%s: [%s] - %s%n", timeFormatter.format(now), threadName, message);
   }
 
 }
