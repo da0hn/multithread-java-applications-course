eac32902c900c3f904192925bfaccef57b5d916e
Da0hn <gabrielhondacba@gmail.com>
2023-04-21 21:31:40 -0400

    feat: fetch inventory data for each product option in product detail

--- basics/product-service/src/main/java/com/da0hn/multithreading/basics/product/service/ProductServiceCompletableFutureImpl.java
+++ basics/product-service/src/main/java/com/da0hn/multithreading/basics/product/service/ProductServiceCompletableFutureImpl.java
@@ -1,19 +1,25 @@
 package com.da0hn.multithreading.basics.product.service;
 
 import com.da0hn.multithreading.basics.product.service.domain.Product;
+import com.da0hn.multithreading.basics.product.service.domain.ProductInfo;
+import com.da0hn.multithreading.basics.product.service.domain.ProductOption;
 import com.da0hn.multithreading.commons.utils.CommonUtil;
 import lombok.AllArgsConstructor;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class ProductServiceCompletableFutureImpl {
 
   private final ProductInfoService productInfoService;
   private final ReviewService reviewService;
+  private final SyncInventoryService syncInventoryService;
+  private final AsyncInventoryService asyncInventoryService;
 
 
-  public Product retrieveProductDetails(final String productId) {
+  public Product syncRetrieveProductDetails(final String productId) {
     CommonUtil.startTimer();
     final var asyncRetrieveProductInfo =
       CompletableFuture.supplyAsync(() -> this.productInfoService.retrieveProductInfo(productId));
@@ -24,7 +30,53 @@ public class ProductServiceCompletableFutureImpl {
       .thenCombine(asyncRetrieveProductInfo, Product.ProductBuilder::productInfo)
       .thenApply(Product.ProductBuilder::build)
       .join();
+
     CommonUtil.timeElapsed();
     return product;
+  }
+
+
+  public CompletableFuture<Product> asyncRetrieveProductDetails(final String productId) {
+    final var asyncRetrieveProductInfo =
+      CompletableFuture.supplyAsync(() -> this.productInfoService.retrieveProductInfo(productId));
+    final var asyncRetrieveReview = CompletableFuture.supplyAsync(() -> this.reviewService.retrieveReview(productId));
+
+    return CompletableFuture.supplyAsync(() -> Product.builder().productId(productId))
+      .thenCombine(asyncRetrieveReview, Product.ProductBuilder::review)
+      .thenCombine(asyncRetrieveProductInfo, Product.ProductBuilder::productInfo)
+      .thenApply(Product.ProductBuilder::build);
+  }
+
+  public Product syncRetrieveProductDetailsWithInventory(final String productId) {
+    CommonUtil.startTimer();
+    final var asyncRetrieveReview = CompletableFuture.supplyAsync(() -> this.reviewService.retrieveReview(productId));
+
+    final var asyncRetrieveProductInfo =
+      CompletableFuture.supplyAsync(() -> this.productInfoService.retrieveProductInfo(productId))
+      .thenApply(productInfo -> {
+        final var productOptions = this.syncUpdateInventory(productInfo); // blocking call
+        return productInfo.toBuilder()
+          .productOptions(productOptions)
+          .build();
+      });
+
+    final var product = CompletableFuture.supplyAsync(() -> Product.builder().productId(productId))
+      .thenCombine(asyncRetrieveReview, Product.ProductBuilder::review)
+      .thenCombine(asyncRetrieveProductInfo, Product.ProductBuilder::productInfo)
+      .thenApply(Product.ProductBuilder::build)
+      .join();
+
+    CommonUtil.timeElapsed();
+    return product;
+  }
+
+  private List<ProductOption> syncUpdateInventory(final ProductInfo productInfo) {
+    return productInfo.productOptions().stream()
+      .map(option -> {
+        final var inventory = this.syncInventoryService.addInventory(option);
+        return option.toBuilder()
+          .inventory(inventory)
+          .build();
+      }).collect(Collectors.toList());
   }
 }
--- basics/product-service/src/main/java/com/da0hn/multithreading/basics/product/service/application/ProductServiceApplication.java
+++ basics/product-service/src/main/java/com/da0hn/multithreading/basics/product/service/application/ProductServiceApplication.java
@@ -1,10 +1,7 @@
 package com.da0hn.multithreading.basics.product.service.application;
 
 
-import com.da0hn.multithreading.basics.product.service.ProductInfoService;
-import com.da0hn.multithreading.basics.product.service.ProductServiceCompletableFutureImpl;
-import com.da0hn.multithreading.basics.product.service.ProductServiceExecutorServiceImpl;
-import com.da0hn.multithreading.basics.product.service.ReviewService;
+import com.da0hn.multithreading.basics.product.service.*;
 import com.da0hn.multithreading.commons.utils.LoggerUtil;
 import lombok.SneakyThrows;
 
@@ -17,8 +14,10 @@ public final class ProductServiceApplication {
   public static void main(final String[] args) {
     final var productInfoService = new ProductInfoService();
     final var reviewService = new ReviewService();
-    final var productService = new ProductServiceCompletableFutureImpl(productInfoService, reviewService);
-    final var product = productService.retrieveProductDetails("05041620-5731-4b7e-a0d9-1891c5800382");
+    final var syncInventoryService = new SyncInventoryService();
+    final var asyncInventoryService = new AsyncInventoryService();
+    final var productService = new ProductServiceCompletableFutureImpl(productInfoService, reviewService, syncInventoryService, asyncInventoryService);
+    final var product = productService.syncRetrieveProductDetailsWithInventory("05041620-5731-4b7e-a0d9-1891c5800382");
     LoggerUtil.log(product);
   }
 
